f8568148c7e8588b88c3c75acc96ba40d7a10479
Kaleb Kougl <kalebkougl@gmail.com>
2019-01-29 07:39:42 -0600

    grab info onsubmission and dispatch an action

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -13,7 +13,7 @@ const Single = React.createClass({
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props} />
-        <Comments postComments={comments} />
+        <Comments postComments={comments} {...this.props} />
       </div>
     );
   }
